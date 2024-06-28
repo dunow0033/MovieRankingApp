e7e01fe00a2cc465e6ac2241e1f42a528384fe34
Brian Donnoe <dunow33@hotmail.com>
2024-06-27 22:49:58 -0400

    trying to get some files added to this commit

--- movie-ranking-ui/src/components/RankItems.js
+++ movie-ranking-ui/src/components/RankItems.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import MovieImageArr from "./MovieImages.js";
+import RankingGrid from "./RankingGrid.js";
+import '../custom.css';
 
 const RankItems = () => {
     const [items, setItems] = useState([]);
     const dataType = 1;
 
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if(targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if(targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ? 
+                {...item, ranking:parseInt(targetElm.id.substring(5))} : {...item, ranking: item.ranking });
+                setItems(transformedCollection);
+        }
+    }
+
     useEffect(() => {
-        fetch("http://localhost:5160/ItemModels").then((results) => {
+        fetch("http://localhost:5160/ItemModels")
+            .then((results) => {
             return results.json();
         })
         .then(data => {
@@ -16,13 +41,19 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+            <div className="items-not-ranked">
             {
+                
                 (items.length > 0) ? items.map((item) => 
-                
-                <h3>{item.title}</h3>
-                
+                    <div className="unranked-cell">
+                        <img id={`item-${item.id}`} src={ MovieImageArr.find(o => o.id === item.imageId)?.image }
+                            style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+                        />
+                    </div>
                 ) : <div> Loading... </div>
             }
+            </div>
         </main>
     )
 }
